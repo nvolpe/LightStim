90f5da5f95d3fd4ce48824c3790ee4afa88ed0b8
nvolpe <nvolpe@dtsagile.com>
2015-03-22 21:28:42 -0600

    rocking countdown timer basics

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -1,110 +1,119 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['timer'])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  // Form data for the login modal
-  /*
-  $scope.loginData = {};
+.controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
+    // Form data for the login modal
+    /*
+    $scope.loginData = {};
+  
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+    */
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-  */
+    // Triggered in the login modal to close it
+    /*
+    $scope.closeLogin = function() {
+      $scope.modal.hide();
+    };
+    */
 
-  // Triggered in the login modal to close it
-  /*
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-  */
+    // Open the Concierge_Wrinkles modal
+    /*
+    $scope.Concierge_Wrinkles = function() {
+      $scope.modal.show();
+    };
+    */
 
-  // Open the Concierge_Wrinkles modal
-  /*
-  $scope.Concierge_Wrinkles = function() {
-    $scope.modal.show();
-  };
-  */
-    
-  // Create the Concierge_Wrinkles modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/concierge_wrinkles.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+    // Create the Concierge_Wrinkles modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/concierge_wrinkles.html', {
+        scope: $scope
+    }).then(function (modal) {
+        $scope.modal = modal;
+    });
 
-  // Triggered in the Concierge_Wrinkles modal to close it
-  $scope.closeConcierge_Wrinkles = function() {
-    $scope.modal.hide();
-  };
+    // Triggered in the Concierge_Wrinkles modal to close it
+    $scope.closeConcierge_Wrinkles = function () {
+        $scope.modal.hide();
+    };
 
-  // Open the Concierge_Wrinkles modal
-  $scope.Concierge_Wrinkles = function() {
-    $scope.modal.show();
-  };
+    // Open the Concierge_Wrinkles modal
+    $scope.Concierge_Wrinkles = function () {
+        $scope.modal.show();
+    };
 
 
-  // Perform the login action when the user submits the login form
-  /*
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    //temporary place for now
+    console.log('Start writing code here');
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-  */
+    $scope.timerRunning = false;
+
+
+
+
+
+    // Perform the login action when the user submits the login form
+    /*
+    $scope.doLogin = function() {
+      console.log('Doing login', $scope.loginData);
+  
+      // Simulate a login delay. Remove this and replace with your login
+      // code if using a login system
+      $timeout(function() {
+        $scope.closeLogin();
+      }, 1000);
+    };
+    */
 })
 
 
 
 
-.controller('AppCtrl2', function($scope, $ionicModal, $timeout) {
-    
-    
-  // Create the Concierge_Acne_Mini modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/concierge_acne.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+.controller('AppCtrl2', function ($scope, $ionicModal, $timeout) {
 
-  // Triggered in the Concierge_Acne_Mini modal to close it
-  $scope.closeConcierge_Acne_Mini = function() {
-    $scope.modal.hide();
-  };
 
-  // Open the Concierge_Acne_Mini modal
-  $scope.Concierge_Acne_Mini = function() {
-    $scope.modal.show();
-  };
+    // Create the Concierge_Acne_Mini modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/concierge_acne.html', {
+        scope: $scope
+    }).then(function (modal) {
+        $scope.modal = modal;
+    });
+
+    // Triggered in the Concierge_Acne_Mini modal to close it
+    $scope.closeConcierge_Acne_Mini = function () {
+        $scope.modal.hide();
+    };
+
+    // Open the Concierge_Acne_Mini modal
+    $scope.Concierge_Acne_Mini = function () {
+        $scope.modal.show();
+    };
 
 })
 
 
 
-.controller('AppCtrl3', function($scope, $ionicModal, $timeout) {
-    
-    
-  // Create the Concierge_Acne_Mini modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/concierge_pain.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+.controller('AppCtrl3', function ($scope, $ionicModal, $timeout) {
 
-  // Triggered in the Concierge_Acne_Mini modal to close it
-  $scope.closeConcierge_Pain = function() {
-    $scope.modal.hide();
-  };
 
-  // Open the Concierge_Acne_Mini modal
-  $scope.Concierge_Pain = function() {
-    $scope.modal.show();
-  };
+    // Create the Concierge_Acne_Mini modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/concierge_pain.html', {
+        scope: $scope
+    }).then(function (modal) {
+        $scope.modal = modal;
+    });
+
+    // Triggered in the Concierge_Acne_Mini modal to close it
+    $scope.closeConcierge_Pain = function () {
+        $scope.modal.hide();
+    };
+
+    // Open the Concierge_Acne_Mini modal
+    $scope.Concierge_Pain = function () {
+        $scope.modal.show();
+    };
 
 })
 
