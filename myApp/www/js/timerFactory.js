6bb7ed5ecf0a7eb2d77c7300e938b7ea1f59be3d
nvolpe <nvolpe@dtsagile.com>
2015-03-28 23:15:40 -0600

    da fcuk

---
+++ myApp/www/js/timerFactory.js
@@ -0,0 +1,52 @@
+﻿angular.module('starter')
+
+.factory('timerFactory', function ($log, $cordovaLocalNotification, $ionicPopup) {
+
+    //==============================
+    // Private
+    //==============================
+
+    //TODO: create id rather than pass it in
+    function _setNotification(id, timeAmount) {
+        var alarmTime = new Date();
+        alarmTime.setMinutes(alarmTime.getMinutes() + timeAmount);
+
+        console.log('set notification');
+
+        $cordovaLocalNotification.add({
+            id: id,
+            date: alarmTime,
+            message: "Time is up!",
+            title: "LightStim"
+        }).then(function () {
+            console.log("The notification was set");
+        });
+    };
+
+
+    function _cancelNotification(id) {
+
+        console.log('cancel notification');
+
+        $cordovaLocalNotification.cancel(id).then(function () {
+            console.log('callback for cancellation background notification');
+        });
+    };
+
+    //TODO: Make this smart, we could store a hash of types with numbers,
+    //then we would always be able to stop, start, pause - and update the location notification id
+    //we would have to cancel / reset, but could be sexy
+    function createId(type) {
+
+    };
+
+    //==============================
+    // Public Api
+    //==============================
+    return {
+        setNotification: _setNotification,
+        cancelNotification: _cancelNotification,
+    };
+
+
+});
