33b59327f140f3f9a7aea073d731f4e8112a6bd7
nvolpe <nvolpe@dtsagile.com>
2015-04-01 21:38:20 -0600

    adding some hackish things for placing the slider handle

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -28,9 +28,9 @@ angular.module('starter.controllers', ['timer'])
 
 
 //==============================
-// Wrinkle Controller
+// Acne Controller
 //==============================
-.controller('wrinklesCtrl', function ($scope, $rootScope, $ionicModal, $timeout, timerFactory, $ionicSideMenuDelegate) {
+.controller('acneMiniCtrl', function ($scope, $rootScope, $ionicModal, $timeout, timerFactory, $ionicSideMenuDelegate) {
 
     // Create the Concierge_Acne_Mini modal that we will use later
     $ionicModal.fromTemplateUrl('templates/concierge_wrinkles.html', {
@@ -53,11 +53,10 @@ angular.module('starter.controllers', ['timer'])
     //Nick V's Code
     //--------------------------------------------
 
-    $scope.timeAmount = 180; // default the timer to 3 minutes
+    $scope.timeAmount = 300; // default the timer to 3 minutes
     $scope.timerRunning = false;
     $scope.tester = 'test';
-    $scope.alarmmMinutes = 3;
-    $scope.selectedTime = { "color": "red" };
+    $scope.alarmmMinutes = 5;
 
     var id;
     function formatSeconds(isReset) {
@@ -81,20 +80,18 @@ angular.module('starter.controllers', ['timer'])
     function findSliderInterval(val) {
 
         //find the breaks
-        if (val <= 150) {
-            setSlider(120, 2, 1);
-        } else if (val > 150 && val <= 210) {
-            setSlider(180, 3, 2);
-        } else if (val > 210 && val <= 270) {
-            setSlider(240, 4, 3);
-        } else if (val > 270 && val <= 330) {
-            setSlider(300, 5, 4);
-        } else if (val > 330 && val <= 390) {
-            setSlider(360, 6, 5);
+
+        //seems hacky to hardcode these value, need the maths here to make this sexier
+        if (val <= 450) {
+            setSlider(300, 2, 370, 1);
+        } else if (val > 450 && val <= 750) {
+            setSlider(600, 3, 600, 2);
+        } else if (val > 750 && val <= 900) {
+            setSlider(900, 4, 830, 3);
         }
     }
 
-    function setSlider(seconds, minutes, position) {
+    function setSlider(seconds, minutes, value, position) {
 
         //if (adjust) {
         //    adjustedSeconds = seconds + 18;
@@ -102,10 +99,10 @@ angular.module('starter.controllers', ['timer'])
         //    adjustedSeconds = seconds;
         //}
 
-        $('input[type="range"]').val(seconds).change();
-        $('.progress-bar').css("width", 0 + "%").attr("aria-valuenow", 0);
-        $('.col').removeClass('selectedTime');
-        $('#time-row :nth-child(' + position + ')').addClass('selectedTime');
+        $('input[type="range"]').val(value).change();
+        $('.progress-bar-acne').css("width", 0 + "%").attr("aria-valuenow", 0);
+        $('.col').removeClass('selectedTime-acne');
+        $('#time-row :nth-child(' + position + ')').addClass('selectedTime-acne');
 
         $scope.timeAmount = seconds;
         formatSeconds();
@@ -117,7 +114,6 @@ angular.module('starter.controllers', ['timer'])
         if (time) {
             $scope.timeAmount = time;
         }
-
 
         if ($scope.isStarted) {
             formatSeconds(true);
@@ -161,9 +157,9 @@ angular.module('starter.controllers', ['timer'])
         onInit: function () {
             isStart = false;
 
-            $('.rangeslider__fill').addClass('progress-bar');
-            $('.rangeslider__handle').addClass('wrinkle-slider-handle');
-            $('.progress-bar').css("width", 0 + "%").attr("aria-valuenow", 0);
+            $('.rangeslider__fill').addClass('progress-bar-acne');
+            $('.rangeslider__handle').addClass('acne-slider-handle');
+            $('.progress-bar-acne').css("width", 0 + "%").attr("aria-valuenow", 0);
         },
 
         // Callback function
@@ -179,7 +175,6 @@ angular.module('starter.controllers', ['timer'])
 
             console.log('position', position)
             console.log('value', value)
-
        
             findSliderInterval(value);
             resetTimer();
@@ -203,8 +198,8 @@ angular.module('starter.controllers', ['timer'])
 
         //TODO, make these dynamic
         var timer = 0;
-        var maxTime = 360;
-        var minTime = 120;
+        var maxTime = 900;
+        var minTime = 300;
 
         var endTime = $scope.timeAmount;
         var perecentMax = (endTime - minTime) / (maxTime - minTime) * 100;
@@ -219,7 +214,7 @@ angular.module('starter.controllers', ['timer'])
             currentTime = timer / 10;
             progressPercent = (currentTime / endTime) * perecentMax;
             console.log('progressPercent', progressPercent);
-            $('.progress-bar').css("width", progressPercent + "%").attr("aria-valuenow", progressPercent);
+            $('.progress-bar-acne').css("width", progressPercent + "%").attr("aria-valuenow", progressPercent);
 
             if (currentTime >= endTime) {
                 return;
@@ -240,7 +235,7 @@ angular.module('starter.controllers', ['timer'])
             clearTimeout(progressTimer);
 
             //reset the progress bar back to 0 percent
-            $('.progress-bar').css("width", "0%").attr("aria-valuenow", 0);
+            $('.progress-bar-acne').css("width", "0%").attr("aria-valuenow", 0);
         }
     }
 
@@ -250,19 +245,11 @@ angular.module('starter.controllers', ['timer'])
         TODO:
         When the User clicks START TIME, set a local notification for that exact time
     */
-
-    //var timeAmount = 2;
-
-
-  
-
     var minutes;
     var countdownTimer;
 
     var previousSeconds;
     $scope.isStarted = false;
-
-
 
     function startCountdown() {
 
@@ -342,60 +329,311 @@ angular.module('starter.controllers', ['timer'])
 })
 
 
-.controller('acneMiniCtrl', function ($scope, $ionicModal, $ionicPopup, $timeout, timerFactory) {
+//==============================
+// Wrinkle Controller
+//==============================
+.controller('wrinklesCtrl', function ($scope, $rootScope, $ionicModal, $timeout, timerFactory, $ionicSideMenuDelegate) {
 
     // Create the Concierge_Acne_Mini modal that we will use later
-    $ionicModal.fromTemplateUrl('templates/concierge_acne.html', {
+    $ionicModal.fromTemplateUrl('templates/concierge_wrinkles.html', {
         scope: $scope
     }).then(function (modal) {
         $scope.modal = modal;
     });
 
     // Triggered in the Concierge_Acne_Mini modal to close it
-    $scope.closeConcierge_Acne_Mini = function () {
+    $scope.closeConcierge = function () {
         $scope.modal.hide();
     };
 
-    // Open the Concierge_Acne_Mini modal
-    $scope.Concierge_Acne_Mini = function () {
+    // Open the modal
+    $scope.showConcierge = function () {
         $scope.modal.show();
     };
+
 
     //Nick V's Code
     //--------------------------------------------
 
-    var timeStarted = false;
+    $scope.timeAmount = 180; // default the timer to 3 minutes
     $scope.timerRunning = false;
+    $scope.tester = 'test';
+    $scope.alarmmMinutes = 3;
+    $scope.selectedTime = { "color": "red" };
+
+    var id;
+    function formatSeconds(isReset) {
+
+        //if (isReset) {
+        //    $scope.timeAmount = 180;
+        //} 
+
+        var minutes = Math.round(($scope.timeAmount - 30) / 60);
+        var remainingSeconds = $scope.timeAmount % 60;
+        if (remainingSeconds < 10) {
+            remainingSeconds = "0" + remainingSeconds;
+        }
+
+        $scope.minutes = minutes;
+        $scope.seconds = remainingSeconds;
+        $scope.$apply(); //throws an error that $scope.$digest() is already occuring
+    }
+
+
+    function findSliderInterval(val) {
+
+        //find the breaks
+        if (val <= 150) {
+            setSlider(120, 2, 1);
+        } else if (val > 150 && val <= 210) {
+            setSlider(180, 3, 2);
+        } else if (val > 210 && val <= 270) {
+            setSlider(240, 4, 3);
+        } else if (val > 270 && val <= 330) {
+            setSlider(300, 5, 4);
+        } else if (val > 330 && val <= 390) {
+            setSlider(360, 6, 5);
+        }
+    }
+
+    function setSlider(seconds, minutes, position) {
+
+        //if (adjust) {
+        //    adjustedSeconds = seconds + 18;
+        //} else {
+        //    adjustedSeconds = seconds;
+        //}
+
+        $('input[type="range"]').val(seconds).change();
+        $('.progress-bar-wrinkle').css("width", 0 + "%").attr("aria-valuenow", 0);
+        $('.col').removeClass('selectedTime-wrinkle');
+        $('#time-row :nth-child(' + position + ')').addClass('selectedTime-wrinkle');
+
+        $scope.timeAmount = seconds;
+        formatSeconds();
+        $scope.alarmmMinutes = minutes;
+    }
+
+    function resetTimer(time) {
+
+        if (time) {
+            $scope.timeAmount = time;
+        }
+
+
+        if ($scope.isStarted) {
+            formatSeconds(true);
+            $scope.timerRunning = false;
+            stopProgressBar();
+            $timeout.cancel(countdownTimer);
+            //timerFactory.cancelNotification(id);
+            console.log('cancellllllllled');
+        }
+    }
+
+    function createId() {
+        id = Math.floor(Math.random() * (10000));
+    }
+
+
+    formatSeconds();
+    createId();
+    $ionicSideMenuDelegate.canDragContent(true);
+
+    /*
+        BUG: Jquery is breaking the diretives! This shouldnt be written in JQuery anyways!!!
+    */
+
+    //Initiate Range Slider
+    var isStart = false;
+    $('input[type="range"]').rangeslider({
+
+        // Feature detection the default is `true`.
+        // Set this to `false` if you want to use
+        // the polyfill also in Browsers which support
+        // the native <input type="range"> element.
+        polyfill: false,
+
+        // Default CSS classes
+        rangeClass: 'rangeslider',
+        fillClass: 'rangeslider__fill',
+        handleClass: 'rangeslider__handle',
+
+        // Callback function
+        onInit: function () {
+            isStart = false;
+
+            $('.rangeslider__fill').addClass('progress-bar-wrinkle');
+            $('.rangeslider__handle').addClass('wrinkle-slider-handle');
+            $('.progress-bar-wrinkle').css("width", 0 + "%").attr("aria-valuenow", 0);
+        },
+
+        // Callback function
+        onSlide: function (position, value) {
+            if (isStart) {
+                $ionicSideMenuDelegate.canDragContent(false);
+            }
+            isStart = true;
+        },
+
+        // Callback function
+        onSlideEnd: function (position, value) {
+
+            console.log('position', position)
+            console.log('value', value)
+
+
+            findSliderInterval(value);
+            resetTimer();
+            $scope.isStarted = false;
+            formatSeconds(true);
+
+            //wow, moving this to the end of the method works, at the start of the method it doesnt work!
+            $ionicSideMenuDelegate.canDragContent(true);
+        }
+    });
+    //-------------------------------
+    //End Slider Init
+
+    /*
+    * Start Progress Bar
+    */
+
+    //define variable for css timer function so we can cancel it later
+    var progressTimer;
+    function startProgressBar() {
+
+        //TODO, make these dynamic
+        var timer = 0;
+        var maxTime = 360;
+        var minTime = 120;
+
+        var endTime = $scope.timeAmount;
+        var perecentMax = (endTime - minTime) / (maxTime - minTime) * 100;
+
+        var currentTime,
+            progressPercent;
+
+        console.log('perecentMax', perecentMax);
+
+        function timerRun() {
+
+            currentTime = timer / 10;
+            progressPercent = (currentTime / endTime) * perecentMax;
+            console.log('progressPercent', progressPercent);
+            $('.progress-bar-wrinkle').css("width", progressPercent + "%").attr("aria-valuenow", progressPercent);
+
+            if (currentTime >= endTime) {
+                return;
+            }
+            timer++;
+            progressTimer = setTimeout(function () { timerRun() }, 100);
+        }
+        timerRun();
+    }
+
+    /*
+    * Stop Progress Bar
+    */
+    function stopProgressBar() {
+
+        if (progressTimer) {
+            //clear the css timer from updating
+            clearTimeout(progressTimer);
+
+            //reset the progress bar back to 0 percent
+            $('.progress-bar-wrinkle').css("width", "0%").attr("aria-valuenow", 0);
+        }
+    }
+
+    var timeStarted = false;
 
     /*
         TODO:
         When the User clicks START TIME, set a local notification for that exact time
     */
 
-    var timeStarted = false;
-    $scope.timerRunning = false;
+    //var timeAmount = 2;
 
-    var timeAmount = 2;
+    var minutes;
+    var countdownTimer;
 
-    var id = Math.floor(Math.random() * (10000));
-    console.log('id', id);
+    var previousSeconds;
+    $scope.isStarted = false;
 
+
+
+    function startCountdown() {
+
+        if (!$scope.isStarted) {
+            seconds = $scope.timeAmount;
+            $scope.isStarted = true;
+        }
+
+        //var seconds = $scope.timeAmount;
+        if ($scope.isStarted == true) {
+
+            minutes = Math.round((seconds - 30) / 60);
+            var remainingSeconds = seconds % 60;
+            if (remainingSeconds < 10) {
+                remainingSeconds = "0" + remainingSeconds;
+            }
+
+
+            console.debug('minutes', minutes);
+            console.debug('remainingSeconds', remainingSeconds);
+
+            $scope.minutes = minutes;
+            $scope.seconds = remainingSeconds;
+
+            if (seconds == 0) {
+                $timeout.cancel(countdownTimer);
+                $scope.isStarted = false;
+                return;
+            } else {
+                seconds--;
+            }
+
+
+            countdownTimer = $timeout(startCountdown, 1000);
+        }
+    }
+
+    /*
+        ngclick events
+    */
     $scope.startStopTimer = function () {
 
-        $scope.test = { color: "red" };
+        if (!$scope.timerRunning) {
 
-        if (!timeStarted) {
-            $scope.$broadcast('timer-start');
+            countdownTimer = $timeout(startCountdown, 1000);
+
+            // Let's bind to the resolve/reject handlers of
+            // the timer promise so that we can make sure our
+            // cancel approach is actually working.
+            countdownTimer.then(
+                function () {
+                    console.log("Timer resolved!", Date.now());
+                },
+                function () {
+                    console.log("Timer rejected!", Date.now());
+
+                }
+            );
+
             $scope.timerRunning = true;
-            timeStarted = true;
+
+            //start updating the progress bar
+            startProgressBar();
 
             //set local notification
-            timerFactory.setNotification(id, timeAmount); //TODO pass in acne
-        } else {
-            $scope.$broadcast('timer-reset');
-            $scope.timerRunning = false;
-            timeStarted = false;
+            timerFactory.setNotification(id, $scope.alarmmMinutes); //TODO pass in wrinkles
 
+        } else {
+
+            resetTimer();
+            $scope.isStarted = false;
+            formatSeconds(true);
             //cancels local notification
             timerFactory.cancelNotification(id);
         }
