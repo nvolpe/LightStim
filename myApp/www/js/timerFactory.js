a2b4d75ab7ff3d1521570245dc0d94d142463a7b
nvolpe <nvolpe@dtsagile.com>
2015-04-09 00:08:35 -0600

    adjusted timers

--- myApp/www/js/timerFactory.js
+++ myApp/www/js/timerFactory.js
@@ -8,40 +8,87 @@ angular.module('starter')
     var ids = [];
     var alarmTime;
     var additionalTime;
-
-
+    var times = [];
 
     //TODO: create id rather than pass it in
     function _setNotification(unimplented, timeAmount) {
 
         createIds();
+        createTimes();
 
-        additionalTime = timeAmount;
-
-        //for development
-        //alarmTime.setMinutes(alarmTime.getMinutes() + 1);
-
-
-        alarmTime = new Date();;
-        alarmTime.setMinutes(alarmTime.getMinutes() + timeAmount);
-
-        console.log('Time was set for : ' + alarmTime);
-        console.log('addtional time is : ' + additionalTime);
-
-        $cordovaLocalNotification.add({
+        $cordovaLocalNotification.add([{
             id: ids[0],
-            date: alarmTime,
-            message: "Timer has reached interval",
+            date: times[0],
+            message: "Timer has reached interval 1",
             title: "LightStim",
             sound: 'file://mp3/beep.caf'
-        }).then(function () {
-            console.log("The notification was set");
-        });
-
+        },
+        {
+            id: ids[1],
+            date: times[1],
+            message: "Timer has reached interval 2",
+            title: "LightStim",
+            sound: 'file://mp3/beep.caf'
+        },
+        {
+            id: ids[2],
+            date: times[2],
+            message: "Timer has reached interval 3",
+            title: "LightStim",
+            sound: 'file://mp3/beep.caf'
+        },
+        {
+            id: ids[3],
+            date: times[3],
+            message: "Timer has reached interval 4",
+            title: "LightStim",
+            sound: 'file://mp3/beep.caf'
+        },
+        {
+            id: ids[4],
+            date: times[4],
+            message: "Timer has reached interval 5",
+            title: "LightStim",
+            sound: 'file://mp3/beep.caf'
+        },
+        {
+            id: ids[5],
+            date: times[5],
+            message: "Timer has reached interval 6",
+            title: "LightStim",
+            sound: 'file://mp3/beep.caf'
+        },
+        {
+            id: ids[6],
+            date: times[6],
+            message: "Timer has reached interval 7",
+            title: "LightStim",
+            sound: 'file://mp3/beep.caf'
+        },
+        {
+            id: ids[7],
+            date: times[7],
+            message: "Timer has reached interval 8",
+            title: "LightStim",
+            sound: 'file://mp3/beep.caf'
+        },
+        {
+            id: ids[8],
+            date: times[8],
+            message: "Timer has reached interval 9",
+            title: "LightStim",
+            sound: 'file://mp3/beep.caf'
+        },
+        {
+            id: ids[9],
+            date: times[9],
+            message: "Timer has reached interval 10",
+            title: "LightStim",
+            sound: 'file://mp3/beep.caf'
+        },
 
 
          //angular.forEach(ids, function (id) {
-
          //    console.log('set notification');
 
          //    alarmTime = new Date();;
@@ -99,6 +146,25 @@ angular.module('starter')
             id = Math.floor(Math.random() * (10000));
             ids.push(id)
 
+        }
+    };
+
+
+    function createTimes(timeAmount) {
+
+        additionalTime = timeAmount;
+
+        for (i = 0; i < 10; i++) {
+
+            alarmTime = new Date();
+            alarmTime.setMinutes(alarmTime.getMinutes() + additionalTime);
+
+            console.log('Time was set for : ' + alarmTime);
+            console.log('addtional time is : ' + additionalTime);
+
+            times.push(alarmTime)
+
+            additionalTime = additionalTime + timeAmount;
         }
     };
 
